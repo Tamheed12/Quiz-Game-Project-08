fdf038a1269aa05adee54958e782912bea1003cf
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-07-30 18:27:01 +0500

    This is Quiz game  Where The main The run file

---
+++ app.js
@@ -0,0 +1,70 @@
+//i make app.ts file for all code and main file that run my code is index.ts
+import readline from "readline";
+//let shuffle the questan for random order use function and if loop
+export function shuffle(questanShuffle) {
+    //make for loop run til last questan
+    for (let i = questanShuffle.length - 1; i > 0; i--) {
+        //use 2 variable for random order
+        let j = Math.floor(Math.random() * i + 1);
+        [questanShuffle[i], questanShuffle[j]] = [questanShuffle[j], questanShuffle[i]];
+    }
+    return questanShuffle;
+}
+;
+//make function to run quiz 
+export function runQuiz(quiz, quantity) {
+    let readlines = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    readlines.question(`____________________________Quiz : ${quantity}____________________________
+    
+    Press enter to start quiz   `, () => {
+        let youScore = 0;
+        //shuffle questan to random order
+        let shuffleQuestans = shuffle(quiz.questan);
+        //make function to ask Questan
+        function askquestan(index) {
+            //use if for questan pattren
+            if (index < shuffleQuestans.length) {
+                let questan = shuffleQuestans[index];
+                console.log(`
+Questan ${index + 1} : ${questan.questan}`);
+                //make loop for contioun  the questan ask
+                for (let i = 0; i < questan.option.length; i++) {
+                    console.log(`${i + 1}. ${questan.option[i]}`);
+                }
+                ;
+                readlines.question(`            Enter your answer (1,2,3,etc.): `, (Answer) => {
+                    let answerIndex = parseInt(Answer) - 1;
+                    //make if for correct answer and wrong answer
+                    if (answerIndex === questan.correctAnswer) {
+                        console.log(`
+    **********************Your answer is Correct**********************`);
+                        youScore++;
+                    }
+                    else {
+                        console.log(`
+    --------------------Your answer is Incorrect!--------------------
+    Correct answer is =>    '${questan.option[questan.correctAnswer]}'`);
+                    }
+                    ;
+                    askquestan(index + 1);
+                });
+            }
+            else {
+                //make if questan is complete the in close the percentage 
+                readlines.close();
+                let percentage = (youScore / shuffleQuestans.length) * 100;
+                console.log(`
+                            Quiz ${quantity} Complete!
+                            You score ${youScore}/${shuffleQuestans.length}
+                            You gain ${percentage}% Percentage`);
+            }
+            ;
+        }
+        ;
+        askquestan(0);
+    });
+}
+let answers = [];
---
+++ index.js
@@ -0,0 +1,28 @@
+//this is the main file that run the program and alsos you can add questan and option and correct answer
+import { runQuiz } from "./app.js";
+const StartQuiz = {
+    questan: [
+        {
+            questan: 'The smallest Parical in optoin?',
+            option: ['Electron', 'Atom', 'Molecule'],
+            correctAnswer: 0
+        },
+        {
+            questan: 'Who is the writer of `Seven stages of life`?',
+            option: ['Thomes Gray', 'Jhon wilton', 'Willeam Shasper'],
+            correctAnswer: 2
+        },
+        {
+            questan: 'Output is in below?',
+            option: ['Mouse', 'Tv', 'Remote'],
+            correctAnswer: 1
+        },
+        {
+            questan: 'Input is in below?',
+            option: ['Tv', 'Speaker', 'Remote'],
+            correctAnswer: 2
+        }
+    ]
+};
+//Run quiz 
+runQuiz(StartQuiz, 1);
